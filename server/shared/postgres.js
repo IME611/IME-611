231cdae25cf69dacb350e6cfa969d400cf1e024f
IME611 <idan.moshe.einav@gmail.com>
2026-08-12 19:26:40 +0700

    db: centralize resilient postgres connection settings

--- server/shared/postgres.js
+++ server/shared/postgres.js
@@ -1,12 +1,27 @@
 import pg from 'pg';
 const { Pool } = pg;
 let pool;
+
+export function normalizeDatabaseUrl(value){
+  const url=new URL(value);
+  url.searchParams.delete('sslmode');
+  url.searchParams.delete('uselibpqcompat');
+  return url.toString();
+}
+
 export function getDb(){
   const connectionString=process.env.DATABASE_URL;
   if(!connectionString)throw new Error('DATABASE_URL is required');
   if(!pool){
     const ssl=process.env.DATABASE_SSL==='false'?false:{rejectUnauthorized:false};
-    pool=new Pool({connectionString,ssl,max:5});
+    pool=new Pool({
+      connectionString:normalizeDatabaseUrl(connectionString),
+      ssl,
+      max:5,
+      idleTimeoutMillis:10_000,
+      connectionTimeoutMillis:4_000,
+      allowExitOnIdle:true,
+    });
   }
   return pool;
 }
