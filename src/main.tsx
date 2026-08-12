96bac21abaa75d91abca73eae86c570494bd4981
IME611 <idan.moshe.einav@gmail.com>
2026-08-12 18:44:53 +0700

    feat: mount app behind global error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from'react';
 import{createRoot}from'react-dom/client';
 import App from'./app/App';
+import{AppErrorBoundary}from'./core/errors/AppErrorBoundary';
 import'./design/index.css';
 
-createRoot(document.getElementById('root')!).render(<App/>);
+createRoot(document.getElementById('root')!).render(<AppErrorBoundary><App/></AppErrorBoundary>);
