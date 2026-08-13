7b93c7fb6a25118d60aa47168ac8a524a8334dc2
IME611 <idan.moshe.einav@gmail.com>
2026-08-14 01:08:12 +0700

    design: activate layered welcome glass overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,14 @@ import{createRoot}from'react-dom/client';
 import App from'./app/App';
 import{AppErrorBoundary}from'./core/errors/AppErrorBoundary';
 import'./design/index.css';
+import'./design/features/welcome/background.css';
+import'./design/features/welcome/slab.css';
+import'./design/features/welcome/spark-orb.css';
+import'./design/features/welcome/message.css';
+import'./design/features/welcome/message-finish.css';
+import'./design/features/welcome/button-stack.css';
+import'./design/features/welcome/button-surface.css';
+import'./design/features/welcome/compat.css';
+import'./design/features/welcome/responsive.css';
 
 createRoot(document.getElementById('root')!).render(<AppErrorBoundary><App/></AppErrorBoundary>);
